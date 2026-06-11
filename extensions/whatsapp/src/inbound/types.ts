Kaspnov/openclaw

refactor(whatsapp): introduce inbound message contexts

extensions/whatsapp/src/inbound/types.ts
@@ -53,55 +53,162 @@ export type WhatsAppStructuredContactContext = {
   }>;
 };
 
-export type WebInboundMessage = {
+export type WhatsAppInboundEvent = {
   id?: string;
-  from: string; // conversation id: E.164 for direct chats, group JID for groups
-  conversationId: string; // alias for clarity (same as from)
-  to: string;
-  accountId: string;
-  /** Set by the real inbound monitor after access-control / pairing checks pass. */
-  accessControlPassed?: boolean;
-  body: string;
-  pushName?: string;
   timestamp?: number;
-  chatType: "direct" | "group";
-  chatId: string;
-  sender?: WhatsAppIdentity;
-  senderJid?: string;
-  senderE164?: string;
-  senderName?: string;
-  replyTo?: WhatsAppReplyContext;
-  replyToId?: string;
-  replyToBody?: string;
-  replyToSender?: string;
-  replyToSenderJid?: string;
-  replyToSenderE164?: string;
-  groupSubject?: string;
-  groupParticipants?: string[];
-  mentions?: string[];
-  mentionedJids?: string[];
-  self?: WhatsAppSelfIdentity;
-  selfJid?: string | null;
-  selfLid?: string | null;
-  selfE164?: string | null;
-  fromMe?: boolean;
+  isBatched?: boolean;
+};
+
+export type WhatsAppInboundQuote = {
+  context?: WhatsAppReplyContext;
+  id?: string;
+  body?: string;
+  sender?: {
+    displayName?: string;
+    jid?: string;
+    e164?: string;
+  };
+};
+
+export type WhatsAppInboundGroupContext = {
+  subject?: string;
+  participants?: string[];
+  mentions?: {
+    text?: string[];
+    jids?: string[];
+  };
+};
+
+export type WhatsAppInboundPayload = {
+  body: string;
+  media?: {
+    path?: string;
+    type?: string;
+    fileName?: string;
+    url?: string;
+  };
   location?: NormalizedLocation;
-  sendComposing: () => Promise<void>;
-  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
-  sendMedia: (
-    payload: AnyMessageContent,
-    options?: MiscMessageGenerationOptions,
-  ) => Promise<WhatsAppSendResult>;
-  mediaPath?: string;
-  mediaType?: string;
-  mediaFileName?: string;
-  mediaUrl?: string;
   untrustedStructuredContext?: Array<{
     label: string;
     source?: string;
     type?: string;
     payload: unknown;
   }>;
-  wasMentioned?: boolean;
+};
+
+export type WhatsAppInboundPlatform = {
+  chatJid: string;
+  recipientJid: string;
+  sender?: WhatsAppIdentity;
+  senderJid?: string;
+  senderE164?: string;
+  senderName?: string;
+  pushName?: string;
+  self?: WhatsAppSelfIdentity;
+  selfJid?: string | null;
+  selfLid?: string | null;
+  selfE164?: string | null;
+  fromMe?: boolean;
+  sendComposing: () => Promise<void>;
+  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
+  sendMedia: (
+    payload: AnyMessageContent,
+    options?: MiscMessageGenerationOptions,
+  ) => Promise<WhatsAppSendResult>;
+};
+
+type DeprecatedWebInboundMessageFlatAliases = {
+  /** @deprecated Use `event.id`. */
+  id?: string;
+  /** @deprecated Use `platform.recipientJid`. */
+  to: string;
+  /** @deprecated Use `payload.body`. */
+  body: string;
+  /** @deprecated Use `platform.pushName`. */
+  pushName?: string;
+  /** @deprecated Use `event.timestamp`. */
+  timestamp?: number;
+  /** @deprecated Use `platform.chatJid`. */
+  chatId: string;
+  /** @deprecated Use `platform.sender`. */
+  sender?: WhatsAppIdentity;
+  /** @deprecated Use `platform.senderJid`. */
+  senderJid?: string;
+  /** @deprecated Use `platform.senderE164`. */
+  senderE164?: string;
+  /** @deprecated Use `platform.senderName`. */
+  senderName?: string;
+  /** @deprecated Use `quote.context`. */
+  replyTo?: WhatsAppReplyContext;
+  /** @deprecated Use `quote.id`. */
+  replyToId?: string;
+  /** @deprecated Use `quote.body`. */
+  replyToBody?: string;
+  /** @deprecated Use `quote.sender.displayName`. */
+  replyToSender?: string;
+  /** @deprecated Use `quote.sender.jid`. */
+  replyToSenderJid?: string;
+  /** @deprecated Use `quote.sender.e164`. */
+  replyToSenderE164?: string;
+  /** @deprecated Use `group.subject`. */
+  groupSubject?: string;
+  /** @deprecated Use `group.participants`. */
+  groupParticipants?: string[];
+  /** @deprecated Use `group.mentions.jids`. */
+  mentions?: string[];
+  /** @deprecated Use `group.mentions.jids`. */
+  mentionedJids?: string[];
+  /** @deprecated Use `platform.self`. */
+  self?: WhatsAppSelfIdentity;
+  /** @deprecated Use `platform.selfJid`. */
+  selfJid?: string | null;
+  /** @deprecated Use `platform.selfLid`. */
+  selfLid?: string | null;
+  /** @deprecated Use `platform.selfE164`. */
+  selfE164?: string | null;
+  /** @deprecated Use `platform.fromMe`. */
+  fromMe?: boolean;
+  /** @deprecated Use `payload.location`. */
+  location?: NormalizedLocation;
+  /** @deprecated Use `platform.sendComposing`. */
+  sendComposing: () => Promise<void>;
+  /** @deprecated Use `platform.reply`. */
+  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
+  /** @deprecated Use `platform.sendMedia`. */
+  sendMedia: (
+    payload: AnyMessageContent,
+    options?: MiscMessageGenerationOptions,
+  ) => Promise<WhatsAppSendResult>;
+  /** @deprecated Use `payload.media.path`. */
+  mediaPath?: string;
+  /** @deprecated Use `payload.media.type`. */
+  mediaType?: string;
+  /** @deprecated Use `payload.media.fileName`. */
+  mediaFileName?: string;
+  /** @deprecated Use `payload.media.url`. */
+  mediaUrl?: string;
+  /** @deprecated Use `payload.untrustedStructuredContext`. */
+  untrustedStructuredContext?: Array<{
+    label: string;
+    source?: string;
+    type?: string;
+    payload: unknown;
+  }>;
+  /** @deprecated Use `event.isBatched`. */
   isBatched?: boolean;
 };
+
+export type WebInboundMessage = {
+  event: WhatsAppInboundEvent;
+  payload: WhatsAppInboundPayload;
+  platform: WhatsAppInboundPlatform;
+  from: string; // conversation id: E.164 for direct chats, group JID for groups
+  conversationId: string; // alias for clarity (same as from)
+  accountId: string;
+  /** Set by the real inbound monitor after access-control / pairing checks pass. */
+  accessControlPassed?: boolean;
+  chatType: "direct" | "group";
+  quote?: WhatsAppInboundQuote;
+  group?: WhatsAppInboundGroupContext;
+  wasMentioned?: boolean;
+} & DeprecatedWebInboundMessageFlatAliases;

clawsouls/soulclaw

extensions/whatsapp/src/inbound/types.ts
@@ -54,55 +54,178 @@ export type WhatsAppStructuredContactContext = {
   }>;
 };
 
-export type WebInboundMessage = {
+export type WhatsAppInboundEvent = {
   id?: string;
-  from: string; // conversation id: E.164 for direct chats, group JID for groups
-  conversationId: string; // alias for clarity (same as from)
-  to: string;
-  accountId: string;
-  /** Set by the real inbound monitor after access-control / pairing checks pass. */
-  accessControlPassed?: boolean;
-  body: string;
-  pushName?: string;
   timestamp?: number;
-  chatType: "direct" | "group";
-  chatId: string;
-  sender?: WhatsAppIdentity;
-  senderJid?: string;
-  senderE164?: string;
-  senderName?: string;
-  replyTo?: WhatsAppReplyContext;
-  replyToId?: string;
-  replyToBody?: string;
-  replyToSender?: string;
-  replyToSenderJid?: string;
-  replyToSenderE164?: string;
-  groupSubject?: string;
-  groupParticipants?: string[];
-  mentions?: string[];
-  mentionedJids?: string[];
-  self?: WhatsAppSelfIdentity;
-  selfJid?: string | null;
-  selfLid?: string | null;
-  selfE164?: string | null;
-  fromMe?: boolean;
+  isBatched?: boolean;
+};
+
+export type WhatsAppInboundQuote = {
+  context?: WhatsAppReplyContext;
+  id?: string;
+  body?: string;
+  sender?: {
+    displayName?: string;
+    jid?: string;
+    e164?: string;
+  };
+};
+
+export type WhatsAppInboundGroupContext = {
+  subject?: string;
+  participants?: string[];
+  mentions?: {
+    text?: string[];
+    jids?: string[];
+  };
+};
+
+export type WhatsAppInboundPayload = {
+  body: string;
+  media?: {
+    path?: string;
+    type?: string;
+    fileName?: string;
+    url?: string;
+  };
   location?: NormalizedLocation;
-  sendComposing: () => Promise<void>;
-  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
-  sendMedia: (
-    payload: AnyMessageContent,
-    options?: MiscMessageGenerationOptions,
-  ) => Promise<WhatsAppSendResult>;
-  mediaPath?: string;
-  mediaType?: string;
-  mediaFileName?: string;
-  mediaUrl?: string;
   untrustedStructuredContext?: Array<{
     label: string;
     source?: string;
     type?: string;
     payload: unknown;
   }>;
-  wasMentioned?: boolean;
+};
+
+export type WhatsAppInboundPlatform = {
+  chatJid: string;
+  recipientJid: string;
+  sender?: WhatsAppIdentity;
+  senderJid?: string;
+  senderE164?: string;
+  senderName?: string;
+  pushName?: string;
+  self?: WhatsAppSelfIdentity;
+  selfJid?: string | null;
+  selfLid?: string | null;
+  selfE164?: string | null;
+  fromMe?: boolean;
+  sendComposing: () => Promise<void>;
+  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
+  sendMedia: (
+    payload: AnyMessageContent,
+    options?: MiscMessageGenerationOptions,
+  ) => Promise<WhatsAppSendResult>;
+};
+
+export type DeprecatedWebInboundMessageFlatAliases = {
+  /** @deprecated Use `event.id`. */
+  id?: string;
+  /** @deprecated Use `platform.recipientJid`. */
+  to: string;
+  /** @deprecated Use `payload.body`. */
+  body: string;
+  /** @deprecated Use `platform.pushName`. */
+  pushName?: string;
+  /** @deprecated Use `event.timestamp`. */
+  timestamp?: number;
+  /** @deprecated Use `platform.chatJid`. */
+  chatId: string;
+  /** @deprecated Use `platform.sender`. */
+  sender?: WhatsAppIdentity;
+  /** @deprecated Use `platform.senderJid`. */
+  senderJid?: string;
+  /** @deprecated Use `platform.senderE164`. */
+  senderE164?: string;
+  /** @deprecated Use `platform.senderName`. */
+  senderName?: string;
+  /** @deprecated Use `quote.context`. */
+  replyTo?: WhatsAppReplyContext;
+  /** @deprecated Use `quote.id`. */
+  replyToId?: string;
+  /** @deprecated Use `quote.body`. */
+  replyToBody?: string;
+  /** @deprecated Use `quote.sender.displayName`. */
+  replyToSender?: string;
+  /** @deprecated Use `quote.sender.jid`. */
+  replyToSenderJid?: string;
+  /** @deprecated Use `quote.sender.e164`. */
+  replyToSenderE164?: string;
+  /** @deprecated Use `group.subject`. */
+  groupSubject?: string;
+  /** @deprecated Use `group.participants`. */
+  groupParticipants?: string[];
+  /** @deprecated Use `group.mentions.jids`. */
+  mentions?: string[];
+  /** @deprecated Use `group.mentions.jids`. */
+  mentionedJids?: string[];
+  /** @deprecated Use `platform.self`. */
+  self?: WhatsAppSelfIdentity;
+  /** @deprecated Use `platform.selfJid`. */
+  selfJid?: string | null;
+  /** @deprecated Use `platform.selfLid`. */
+  selfLid?: string | null;
+  /** @deprecated Use `platform.selfE164`. */
+  selfE164?: string | null;
+  /** @deprecated Use `platform.fromMe`. */
+  fromMe?: boolean;
+  /** @deprecated Use `payload.location`. */
+  location?: NormalizedLocation;
+  /** @deprecated Use `platform.sendComposing`. */
+  sendComposing: () => Promise<void>;
+  /** @deprecated Use `platform.reply`. */
+  reply: (text: string, options?: MiscMessageGenerationOptions) => Promise<WhatsAppSendResult>;
+  /** @deprecated Use `platform.sendMedia`. */
+  sendMedia: (
+    payload: AnyMessageContent,
+    options?: MiscMessageGenerationOptions,
+  ) => Promise<WhatsAppSendResult>;
+  /** @deprecated Use `payload.media.path`. */
+  mediaPath?: string;
+  /** @deprecated Use `payload.media.type`. */
+  mediaType?: string;
+  /** @deprecated Use `payload.media.fileName`. */
+  mediaFileName?: string;
+  /** @deprecated Use `payload.media.url`. */
+  mediaUrl?: string;
+  /** @deprecated Use `payload.untrustedStructuredContext`. */
+  untrustedStructuredContext?: Array<{
+    label: string;
+    source?: string;
+    type?: string;
+    payload: unknown;
+  }>;
+  /** @deprecated Use `event.isBatched`. */
   isBatched?: boolean;
 };
+
+type WebInboundMessageCommon = {
+  from: string; // conversation id: E.164 for direct chats, group JID for groups
+  conversationId: string; // alias for clarity (same as from)
+  accountId: string;
+  /** Set by the real inbound monitor after access-control / pairing checks pass. */
+  accessControlPassed?: boolean;
+  chatType: "direct" | "group";
+  quote?: WhatsAppInboundQuote;
+  group?: WhatsAppInboundGroupContext;
+  wasMentioned?: boolean;
+};
+
+export type WebInboundCallbackMessage = WebInboundMessageCommon & {
+  event: WhatsAppInboundEvent;
+  payload: WhatsAppInboundPayload;
+  platform: WhatsAppInboundPlatform;
+};
+
+export type WebInboundMessage = WebInboundCallbackMessage & DeprecatedWebInboundMessageFlatAliases;
+
+export type LegacyFlatWebInboundMessage = WebInboundMessageCommon &
+  DeprecatedWebInboundMessageFlatAliases & {
+    event?: never;
+    payload?: never;
+    platform?: never;
+    quote?: never;
+    group?: never;
+  };
+
+export type WebInboundMessageInput = LegacyFlatWebInboundMessage | WebInboundCallbackMessage;